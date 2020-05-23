ddd3987fc416fc9659fab7b0318ef31a3daa1f2a
Coleman Imhoff <colemanimhoff@gmail.com>
2020-05-22 18:37:00 -0600

    setting up app to deploy to heroku

---
+++ public/precache-manifest.fd89c021382e16c6a1b2d4f1cda0216b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70d105523c340a448c64ed034196ca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aecd0e300f80ad5bbc0",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "863a59ede416ea21c22c",
+    "url": "/static/js/2.770573d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.770573d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aecd0e300f80ad5bbc0",
+    "url": "/static/js/main.38b467eb.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
