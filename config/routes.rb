c31d22ff81ccca7cb7dfb0e8e266bfc183cbad13
Coleman Imhoff <colemanimhoff@gmail.com>
2020-05-22 21:24:04 -0600

    fall back option for react router

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :todos
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
